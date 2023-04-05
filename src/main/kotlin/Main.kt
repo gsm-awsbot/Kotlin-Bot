93d229f72e33a1dde4b1c9af27459135629189f6
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-05 09:35:09 +0900

    디스코드 봇 sample 코드 작성

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,57 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.jessecorbett.diskord.bot.bot
+import com.jessecorbett.diskord.bot.classicCommands
+import com.jessecorbett.diskord.bot.events
+import com.jessecorbett.diskord.bot.interaction.interactions
+import com.jessecorbett.diskord.util.sendMessage
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+suspend fun main(args: Array<String>) {
+    sampleInteractionsBot()
+//    sampleEventBot()
+//    sampleClassicCommands()
+}
+
+private suspend fun sampleInteractionsBot() {
+    bot(TOKEN) {
+        interactions {
+            slashCommand("echo", "따라 말하기") {
+                val message by stringParameter("message", "하고 싶은 말 입력")
+                callback { interaction, _ ->
+                    interaction.respond {
+                        content = message
+                    }
+                }
+            }
+
+            slashCommand("introduce", "자기소개") {
+                callback { interaction, _ ->
+                    interaction.respond {
+                        content = "저는 백승민님의 충실한 하인 채종인입니다."
+                    }
+                }
+            }
+        }
+    }
+}
+
+private suspend fun sampleEventBot() {
+    bot(TOKEN) {
+        events {
+            onGuildMemberAdd {
+                channel("").sendMessage("Welcome to the server, ${it.user?.username}!")
+            }
+            onChannelCreate {
+                channel(it.id).sendMessage("채널 생성했네")
+            }
+        }
+    }
+}
+
+private suspend fun sampleClassicCommands() {
+    bot(TOKEN) {
+        classicCommands {
+            command("ping") {
+                it.respond("pong")
+            }
+        }
+    }
 }
